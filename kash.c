f42fdd69df0fd199755755d6ebed4073f3f00fe7
Keith Yong <terda12@gmail.com>
2014-09-17 11:40:21 -0400

    Added super basic functionality

--- kash.c
+++ kash.c
@@ -7,13 +7,24 @@
 #include <sys/wait.h> 
 #include <errno.h> 
 #include <signal.h>
+#include <dirent.h>
 
-char line[255];
+char *line;
+size_t bufferSize = 1024;
+
+char *output;
 
 int main(){
     while(1){
-        printf("ka$h >");
-        getline(line);
-        printf(line);
+        printf("kash> ");
+        getline(&line, &bufferSize, stdin);
+        if (!strcmp(line, "exit\n")){
+            exit(1);
+        }
+        else if (!strcmp(line, "ls\n")){
+            opendir(output);
+            printf("%s", output);
+        }
+        printf("%s", line);
     }
 }
