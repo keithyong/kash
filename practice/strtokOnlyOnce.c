9e732835ab7c9f4e5c21be15ebf988132594a1fd
Keith Yong <terda12@gmail.com>
2014-10-07 09:28:55 -0400

    Got strtokOnlyOnce.c to work as intended, after debugging for a long time

--- practice/strtokOnlyOnce.c
+++ practice/strtokOnlyOnce.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#define BUFFER      1024
 struct redirCode findRedirects(char *input);
 struct redirCode {
     /* For code:
@@ -13,19 +15,18 @@ struct redirCode {
 };
 
 int main(){
-	const char *delims = "<>";
+    const char *delims = "<>";
     struct redirCode temp;
+    char line[BUFFER];
 
-    char *line;
-
+    printf("Input: ");
     fgets(line, 1024, stdin);
     temp = findRedirects(line);
 
-    printf("temp:\n");
     printf("temp.code = %d\n", temp.code);
     printf("temp.fileName = %s\n", temp.fileName);
     printf("temp.argsWithoutFile = %s\n", temp.argsWithoutFile);
-    
+
 }
 
 /* Looks for '>', '<' in a string.
@@ -39,27 +40,40 @@ int main(){
 struct redirCode findRedirects(char *input)
 {
     const char *delims = "<>";
-    struct redirCode redirToReturn;
+    struct redirCode *redirToReturn = malloc(sizeof(struct redirCode));
 
     //Do an initial search for the delimeters
     //before strtok destroys it. O(n) time.
-    int i;
     int redirectOperatorReached = 0;
     int count = 0;
+
+    int i;
     for (i = 0; input[i] != 0; i++){
         if (input[i] == '<'){
-            redirToReturn.code = 1;
+            redirToReturn->code = 1;
             redirectOperatorReached = 1;
+            input[i] = ' ';
         }
         else if (input[i] == '>'){
-            redirToReturn.code = 2;
+            redirToReturn->code = 2;
             redirectOperatorReached = 1;
+            input[i] = ' ';
         }
-        if (redirectOperatorReached == 0){
-        	count++;
+        if (redirectOperatorReached != 1){
+            count++;
         }
     }
-    strncpy(redirToReturn.argsWithoutFile, input + 10, i);
+    int lengthOfInput = strlen(input);
 
-    return redirToReturn;
+    int sizeOfMalloc = (lengthOfInput+1)*sizeof(char);
+    redirToReturn->argsWithoutFile = (char *) malloc(sizeOfMalloc);
+    redirToReturn->fileName = (char *) malloc(sizeOfMalloc);
+
+    strncpy(redirToReturn->argsWithoutFile, input, count);
+    redirToReturn->argsWithoutFile[count] = '\0';
+
+    strncpy(redirToReturn->fileName, input + count, lengthOfInput - count);
+
+    return *redirToReturn;
 }
+
